a4194f8ce757795a73ae327a10f8de2585c02556
cocoliino <coralena@gmx.de>
2026-03-27 08:43:12 +0100

    delete file

--- 8 Next.R
+++
@@ -1,8 +0,0 @@
-# 8 Multi-Chorus Subgraph Analysis
-#
-# goal:
-# for every play with more than one chorus node, extract the induced subgraph
-# of just the chorus nodes, then analyse the internal structure:
-# - do chorus nodes actually co-occur (share scenes)?
-# - how strong are inter-chorus edges vs chorus-character edges?
-# - what is the density/topology of the chorus-only subgraph?
